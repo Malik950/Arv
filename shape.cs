e50fcd3ddd9bb7d0d90d34fb2fff1c1d95561e4f
Malik950 <54435682+Malik950@users.noreply.github.com>
2020-09-01 10:53:31 +0200

    börjat

---
+++ shape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class shape
+    {
+        protected int width;
+        protected int height;
+
+        public shape(int w, int h)
+        {
+            this.width = w;
+            this.height = h;
+        }
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+
+        }
+        public int Height
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+
+    }
+}
